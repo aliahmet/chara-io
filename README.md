c10be25a35decd3ec7a65e26ed295a80b6ad3fea
Ahmet Bingul <abingu13@ku.edu.tr>
2016-12-10 20:47:02 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Chatra.io
+
+## About
+Basic chatra.io Python Client.
+
+It's a tool we developed to use  [Chatra.io](https://chatra.io/)  with our services and we wanted to share it with the rest of the world
+
+## Usage
+
+This is a single file. We didn't make it into a package. You can simply copy it and use it. Only dependency is [Requests](http://docs.python-requests.org/en/master/). Which you can install as
+
+```bash
+$ pip install requests
+```
+
+## Coverage
+* Authentication (Basic / Token)
+* Get/Send/Update/Delete a message
+
+Doesn't cover events.
+
+## Legal, Licence & Disclaimer
+This project is in no way affiliated with, authorized, maintained, sponsored or endorsed by [Chatra.io](https://chatra.io/) or any of its affiliates or subsidiaries. 
+
+Its as is, use it, distribute it. Whatever you do its on you, and have fun !
---
+++ chatra.py
@@ -0,0 +1,72 @@
+from json import dumps
+
+import requests
+
+
+class ChatraException(Exception):
+    pass
+
+
+class ChatraClient():
+    # Initilizing
+    def __init__(self, headers):
+        self.base_api = "https://app.chatra.io/api"
+        self.headers = headers
+
+    @classmethod
+    def basic_authentication(cls, username, password):
+        return cls({
+
+            "Content-Type": "application/json",
+            "Authorization": requests.auth._basic_auth_str(username, password)
+        })
+
+    @classmethod
+    def token_authentication(cls, api_key, api_secret):
+        return cls({
+            "Content-Type": "application/json",
+            "Authorization": "Chatra.Simple %s:%s" % (api_key, api_secret)
+
+        })
+
+    def send_pushed_message(self, user_token, text, agent_id=None, group_id=None):
+        payload = {"clientId": user_token, "text": text}
+        if agent_id:
+            payload["agentId"] = agent_id
+        if group_id:
+            payload["groupId"] = group_id
+
+        return self.post("/pushedMessages/", data=payload)
+
+    def get_pushed_message(self, id):
+        return self.get("/pushedMessages/%s")
+
+    def delete_pushed_message(self, id):
+        return self.delete("/pushedMessages/%s")
+
+    def update_pushed_message(self, id, text):
+        payload = {"text": text}
+        return self.get("/pushedMessages/%s", data=payload)
+
+    # Request Methods
+    def post(self, url, data, **kwargs):
+        return self.request("post", url, data=dumps(data), **kwargs)
+
+    def put(self, url, data, **kwargs):
+        return self.request("put", url, data=dumps(data), **kwargs)
+
+    def get(self, url, **kwargs):
+        return self.request("get", url, **kwargs)
+
+    def delete(self, url, **kwargs):
+        return self.request("delete", url, **kwargs)
+
+    # Actual Request call & Headers
+    def request(self, url, data=None, **kwargs):
+        kwargs['headers'] = self.headers
+        url = "".join([self.base_api, url])
+        response = requests.request(url, **kwargs)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ChatraException(response.content)
